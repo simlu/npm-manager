2bdfa10cbb9178babc31eedc365289b55e0f348a
blackflux <blackflux@unfine.de>
2018-07-08 17:01:59 -0700

    feat: added "verify" command

--- src/cmds/persist.js
+++ src/cmds/persist.js
@@ -6,6 +6,7 @@ exports.command = 'persist [pkg]';
 exports.desc = 'Persist Currently Installed Dependency';
 exports.builder = {};
 exports.handler = argv => execSync(
-  fs.readFileSync(path.join(__dirname, "..", "shell", "persist.sh"), "utf8").replace("{{PACKAGE}}", argv.pkg || ""),
+  fs.readFileSync(path.join(__dirname, "..", "shell", "persist.sh"), "utf8")
+    .replace("{{PACKAGE}}", (argv.pkg || "").toLowerCase()),
   { stdio: [0, 1, 2] }
 );
